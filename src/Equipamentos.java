501eb986bebcc9c07a028527b664a7f8f00716f9
Henrique Sousa Brito <henriquesousa5507@gmail.com>
2024-11-19 16:29:18 -0300

    Terminando Modal Equipamentos

--- src/Equipamentos.java
+++ src/Equipamentos.java
@@ -4,11 +4,12 @@ import java.awt.Dimension;
 import java.awt.Font;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
 public class Equipamentos extends JFrame {
   public Equipamentos() {
     JFrame modalEquipamentos = new JFrame("EQUIPAMENTOS");
-    int widthTela = 830;
+    int widthTela = 1000;
 
     // Criando Header
     JLabel titleHeader = new JLabel("Equipamentos", SwingConstants.CENTER);
@@ -45,6 +46,44 @@ public class Equipamentos extends JFrame {
     addEquipamento.setFont(new Font("Arial", Font.BOLD, 18));
     addEquipamento.setForeground(Color.WHITE);
 
+    // Criado listagem de Equipamentos
+    String[] colunas = { "ID", "NOME", "LOCALIDADE" };
+    Object[][] dados = { // Dados que virao ao cadastrar um equipamento
+        { 1, "TV", "Sala" },
+        { 2, "TV", "Sala" },
+        { 3, "TV", "Sala" },
+        { 4, "TV", "Sala" },
+        { 4, "TV", "Sala" },
+        { 5, "TV", "Sala" },
+        { 6, "TV", "Sala" },
+        { 7, "TV", "Sala" },
+        { 8, "TV", "Sala" },
+        { 9, "TV", "Sala" },
+        { 10, "TV", "Sala" },
+        { 11, "TV", "Sala" },
+    };
+    DefaultTableModel modelo = new DefaultTableModel(dados, colunas);
+    JTable tabela = new JTable(modelo);
+    tabela.setDefaultEditor(Object.class, null);
+    tabela.getTableHeader().setReorderingAllowed(false);
+    JScrollPane scrollPane = new JScrollPane(tabela);
+    // estilos tabela
+    tabela.getTableHeader().setFont(new Font("Arial", Font.BOLD, 15));
+    tabela.getTableHeader().setBackground(new Color(70, 130, 180));
+    tabela.getTableHeader().setForeground(Color.WHITE);
+    tabela.setBackground(new Color(245, 245, 245));
+    tabela.setRowHeight(30);
+
+    // Remover equipamento
+    JLabel headerRemove = new JLabel("Remover equipamento pelo ID", SwingConstants.CENTER);
+    headerRemove.setFont(new Font("Arial", Font.BOLD, 18));
+    JTextField idEquipamento = new JTextField();
+    idEquipamento.setFont(new Font("Arial", Font.BOLD, 15));
+    JButton buttonRemove = new JButton("Remover");
+    buttonRemove.setFont(new Font("Arial", Font.BOLD, 18));
+    buttonRemove.setBackground(Color.RED);
+    buttonRemove.setForeground(Color.WHITE);
+
     divAdd.add(title, BorderLayout.CENTER);
     divAdd.add(labelNome, BorderLayout.CENTER);
     divAdd.add(inputNome, BorderLayout.CENTER);
@@ -53,26 +92,35 @@ public class Equipamentos extends JFrame {
     divAdd.add(labelWatts, BorderLayout.CENTER);
     divAdd.add(inputWatts, BorderLayout.CENTER);
     divAdd.add(addEquipamento, BorderLayout.CENTER);
+    divAdd.add(scrollPane);
 
     // Adicionando no Jframe
     modalEquipamentos.add(titleHeader);
     modalEquipamentos.add(divAdd);
+    modalEquipamentos.add(scrollPane);
+    modalEquipamentos.add(headerRemove);
+    modalEquipamentos.add(idEquipamento);
+    modalEquipamentos.add(buttonRemove);
 
     // Aplicando Tamanhos
     titleHeader.setBounds(0, 0, widthTela, 60);
-    divAdd.setBounds(20, 100, widthTela / 2, 400);
-    title.setBounds(0, 0, widthTela / 2, 40);
-    labelNome.setBounds(0, 60, widthTela / 2, 20);
-    inputNome.setBounds(70, 90, widthTela / 3, 30);
-    labelLocal.setBounds(0, 140, widthTela / 2, 20);
-    inputLocal.setBounds(70, 170, widthTela / 3, 30);
-    labelWatts.setBounds(0, 220, widthTela / 2, 20);
-    inputWatts.setBounds(70, 250, widthTela / 3, 30);
-    addEquipamento.setBounds(70, 300, widthTela / 3, 40);
+    divAdd.setBounds(20, 100, (int) (widthTela / 2.2f), 400);
+    title.setBounds(0, 0, (int) (widthTela / 2.2f), 40);
+    labelNome.setBounds(50, 60, widthTela / 3, 20);
+    inputNome.setBounds(100, 90, widthTela / 4, 30);
+    labelLocal.setBounds(50, 140, widthTela / 3, 20);
+    inputLocal.setBounds(100, 170, widthTela / 4, 30);
+    labelWatts.setBounds(50, 220, widthTela / 3, 20);
+    inputWatts.setBounds(100, 250, widthTela / 4, 30);
+    addEquipamento.setBounds(100, 300, widthTela / 4, 40);
+    scrollPane.setBounds(520, 100, (int) (widthTela / 2.2f), 300);
+    headerRemove.setBounds(520, 410, (int) (widthTela / 2.2f), 20);
+    idEquipamento.setBounds(530, 440, 210, 40);
+    buttonRemove.setBounds(750, 440, 210, 40);
 
     modalEquipamentos.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
     modalEquipamentos.setLayout(null);
-    modalEquipamentos.setBounds(800, 100, widthTela, 800);
+    modalEquipamentos.setBounds(800, 100, widthTela, 600);
     modalEquipamentos.setVisible(true);
   }
 }
