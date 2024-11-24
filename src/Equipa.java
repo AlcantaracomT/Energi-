74430af7fb7c847cf4942dbf9ab63fd151c171e9
Camilla Matos <146495739+b-unny@users.noreply.github.com>
2024-11-24 11:33:56 -0300

    Update Equipa.java
    Adicionar um modelo de dados para armazenar o consumo diário de cada equipamento

--- src/Equipa.java
+++ src/Equipa.java
@@ -1,14 +1,20 @@
-class Equipa {
+import java.util.HashMap;
+import java.util.Map;
+
+// Modificação da classe Equipa para incluir consumo diário
+public class Equipa {
     private int id;
     private String nome;
     private String local;
-    private float wats;
+    private float wats;  // Watts/h do equipamento
+    private Map<String, Float> consumoDiario; // Consumo para cada dia da semana
 
     public Equipa(int id, String nome, String local, Float wats) {
         this.id = id;
         this.nome = nome;
         this.local = local;
         this.wats = wats;
+        this.consumoDiario = new HashMap<>();
     }
 
     public String getNomeLocal() {
@@ -31,12 +37,21 @@ class Equipa {
         return wats;
     }
 
+    public void setConsumo(String dia, float consumo) {
+        consumoDiario.put(dia, consumo);
+    }
+
+    public Map<String, Float> getConsumoDiario() {
+        return consumoDiario;
+    }
+
     @Override
     public String toString() {
-        return "Equipamentos{" +
-                "id='" + id + '\'' +
+        return "Equipamento{" +
+                "id=" + id +
                 ", nome='" + nome + '\'' +
-                ", local=" + local +
+                ", local='" + local + '\'' +
+                ", wats=" + wats +
                 '}';
     }
 }
